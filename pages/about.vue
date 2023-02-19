e808c39eb62b420d1f984f8516d50c5f60b70f6a
pyshrekek <76018945+pyshrekek@users.noreply.github.com>
2023-02-18 23:34:48 -0500

    init

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+/about
+<template>
+    <div>
+        <h2>About</h2>
+        <p>hello</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 40px;
+    }
+    p {
+        
+
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>hello</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 40px;
+    }
+    p {
+        
+
+    }
+</style>
