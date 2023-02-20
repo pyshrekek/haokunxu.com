813bccf367fcf7fdb11677508bfe50bf08e2929c
pyshrekek <76018945+pyshrekek@users.noreply.github.com>
2023-02-20 02:01:58 -0500

    center homepg

--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -4,11 +4,22 @@
     <div>
         <h2 class="title">Portfolio</h2>
         <p class="text-center pb-10">My portfolio</p>
-        <div class="grid grid-cols-1 gap-10">
-            <div v-for="item in projects">
-                <ProjectListing :project="item"/>
+        <div class="grid grid-cols-2 gap-10">
+            <div class="grid grid-cols-1 gap-10">
+                <p>Experiences</p>
+                <div v-for="item in experiences">
+                    <ProjectListing :project="item" />
+                </div>
+            </div>
+
+            <div class="grid grid-cols-1 gap-10">
+                <p>Projects</p>
+                <div v-for="item in projects">
+                    <ProjectListing :project="item" />
+                </div>
             </div>
         </div>
+
     </div>
 </template>
 
@@ -17,28 +28,52 @@ export default {
     data() {
         return {
             projects: [
-                { 
-                    position: "Vice President",
+                {
+                    position: "project 1",
                     time: "2022-present",
                     title: "Stuy Fusion",
                     desc: "i do robotics",
                     image: "./img/479.png",
-                }, 
-                { 
+                },
+                {
                     position: "Member",
                     time: "2021-present",
                     title: "Science Olympiad",
                     desc: "scioly",
                     image: "./img/scioly.png",
                 },
-                { 
+                {
                     position: "position",
                     time: "time",
                     title: "title",
                     desc: "desc",
                     image: "",
                 },
-            ]
+            ],
+
+            experiences: [
+                {
+                    position: "Vice President",
+                    time: "2022-present",
+                    title: "Stuy Fusion",
+                    desc: "i do robotics",
+                    image: "./img/479.png",
+                },
+                {
+                    position: "Member",
+                    time: "2021-present",
+                    title: "Science Olympiad",
+                    desc: "scioly",
+                    image: "./img/scioly.png",
+                },
+                {
+                    position: "position",
+                    time: "time",
+                    title: "title",
+                    desc: "desc",
+                    image: "",
+                },
+            ],
         }
     },
 }
